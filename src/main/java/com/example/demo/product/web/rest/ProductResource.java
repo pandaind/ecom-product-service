bd39adb80dc78fe8cd43ee1ed3bf01cadc2079e8
Chittaranjan Panda <19926216+pandaind@users.noreply.github.com>
2021-08-08 11:00:54 +0530

    Updating Product Resources

--- src/main/java/com/example/demo/product/web/rest/ProductResource.java
+++ src/main/java/com/example/demo/product/web/rest/ProductResource.java
@@ -5,7 +5,6 @@ import com.example.demo.product.service.dto.ProductDTO;
 import com.example.demo.product.web.rest.util.ResponseUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
